85104f2421c485f01cd8c63b01e726816f092d58
Ulisses Nascimento <ulisses.melo.nascimento@gmail.com>
2021-01-06 12:30:34 -0300

    [CHORE] - Adicionado validators para CPF e Email

--- src/controllers/ClienteController.js
+++ src/controllers/ClienteController.js
@@ -1,4 +1,6 @@
 const axios = require('axios')
+const  validarCPF  = require('../helpers/validarCPF')
+const validaEmail = require('../helpers/validaEmail')
 const Cliente = require('../models/Cliente')
 const Endereco = require('../models/Endereco')
 
@@ -12,20 +14,28 @@ module.exports = {
                     let cliente = {}
                     let endereco = {}
                     try {
-                        cliente = await Cliente.create({cpf, email, nome})
+                        if(validarCPF(cpf)){
+                            if(validaEmail(email)){
+                                cliente = await Cliente.create({cpf, email, nome})
+                            }else{
+                                return response.status(400).json({'message': 'E-mail inválido'})
+                            }
+                        }else{
+                            return response.status(400).json({'message': 'CPF inválido'})
+                        }
                     } catch (error) {
-                        return response.status(400).json({'Error': 'Cliente já cadastrado'})                   
+                        return response.status(400).json({'message': 'Cliente já cadastrado'})                   
                     }
                     try {
                         let cliente_id = cliente.dataValues.id
                         endereco = await Endereco.create({ cliente_id, cep, logradouro, bairro, localidade, uf})
                     } catch (errr) {
                         Cliente.destroy({where: {'id': cliente.dataValues.id}})
-                        return response.status(400).json({'Error': 'Ocorreu um erro ao salvar o endereço'})
+                        return response.status(400).json({'message': 'Ocorreu um erro ao salvar o endereço'})
                     }
                     return response.status(201).json({cliente, endereco})
                 }else{
-                    return response.status(400).json({'Error': 'O cep é invalido'})
+                    return response.status(400).json({'message': 'O cep é invalido'})
                 }
             })
     },
